a5aa488f6b149af1cb4afa778eb5d4a4aa97b82a
JarekKowalewski <131595716+JarekKowalewski@users.noreply.github.com>
2023-12-05 14:19:16 +0100

    Second update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,8 +6,8 @@ export const Footer = () => {
   return (
     <footer className="footer dark:bg-slate-800">
       <div className="social-media">
-        <a href="https://https://www.facebook.com/jarek.kowalewski.3/"><AiFillFacebook className="footer-icons"/></a>
-        <a href="https://www.https://www.linkedin.com/in/jaroslawkowalewski/"><AiFillLinkedin className="footer-icons"/></a> 
+        <a href="https://www.facebook.com/jarek.kowalewski.3/"><AiFillFacebook className="footer-icons"/></a>
+        <a href="https://www.linkedin.com/in/jaroslawkowalewski/"><AiFillLinkedin className="footer-icons"/></a> 
         <a href="https://www.instagram.com/j_yesterday/"><AiOutlineInstagram className="footer-icons"/></a> 
         <a href="https://github.com/JarekKowalewski"><AiFillGithub className="footer-icons github-ico"/></a> 
       </div>
